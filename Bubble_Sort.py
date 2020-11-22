a3e6b586f17b61f8d956755c7f6ac9171beae722
incraniac <beingincredible31@gmail.com>
2020-11-22 16:05:32 +0530

    First Commit

---
+++ Bubble_Sort.py
@@ -0,0 +1,20 @@
+def bubble_sort (array , order = True) :
+    # length = len (array)
+    if order == True :
+        for i in range (len(array)) :
+            for j in range (0,len(array)-i-1) :
+                if array[j] > array [j+1] :
+                    (array[j],array[j+1]) = (array[j+1],array[j])
+        return array
+
+    elif order == False :
+        for i in range (len(array)) :
+            for j in range (0,len(array)-i-1) :
+                if array[j] < array [j+1] :
+                    (array[j],array[j+1]) = (array[j+1],array[j])
+        return array
+
+
+data = [2,5,4,-1,-9,5,0,-7]
+print (bubble_sort(data))
+print (bubble_sort(data,order=False))
